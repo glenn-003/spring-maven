fc00344668b590ce6c77790e181079145d26895f
glenn-003 <dayapithy@gmail.com>
2026-01-16 11:18:22 +0100

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annonation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,3 +13,11 @@ public class DemoApplication {
 	}
 
 }
+
+@RestController
+class HelloWorldController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World";
+	}
+} 
